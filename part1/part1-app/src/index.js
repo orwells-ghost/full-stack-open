24b198e4fce2dc1f14a920ff1ab5f851b1990d0e
Ryan Kennedy <ryan+github@ryankennedy.me>
2020-03-25 14:06:34 -0700

    Refactor

--- part1/part1-app/src/index.js
+++ part1/part1-app/src/index.js
@@ -1,27 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ handleClick, text }) => (
+	<button onClick={handleClick}>
+		{text}
+	</button>
+)
+
+const App = (props) => {
+	const [ counter, setCounter ] = useState(0)
+
+	const increaseByOne = () => setCounter(counter + 1)
+	const decreaseByOne = () => setCounter(counter - 1)
+	const setToZero = () => setCounter(0)
+
+	return (
+		<div>
+			<Display counter={counter}/>
+			<Button
+				handleClick={increaseByOne}
+				text='plus'
+			/>
+			<Button
+				handleClick={setToZero}
+				text='zero'
+			/>     
+			<Button
+				handleClick={decreaseByOne}
+				text='minus'
+			/>
+		</div>
+	)
 }
 
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+	<App />, 
+	document.getElementById('root')
+)
